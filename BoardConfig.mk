65ac18447e49437d3aafab6dbf53a01bdc1603fc
Rustam Gubaydullin <secondfry@gmail.com>
2016-03-31 15:16:38 +0300

    BoardConfig.mk cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,17 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Board overrides
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_KERNEL     := false
+
 # Board Vendor
 BOARD_VENDOR := sony
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
-TARGET_NO_BOOTLOADER := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 BOARD_VENDOR_PLATFORM := viskan
-TARGET_NO_RADIOIMAGE := true
 
 # Architecture
 TARGET_ARCH := arm
@@ -66,27 +69,20 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP configurations
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
+TW_THEME := portrait_hdpi
 TARGET_RECOVERY_FSTAB := device/sony/huashan/recovery.fstab
+
 TW_BRIGHTNESS_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight1/brightness
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight2/brightness
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/Userdata"
 TW_DEFAULT_BRIGHTNESS := 51
+TW_MAX_BRIGHTNESS := 255
+
+TW_INCLUDE_CRYPTO := true
+
 TW_EXCLUDE_SUPERSU := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_FLASH_FROM_STORAGE := true
-TW_INCLUDE_FUSE_EXFAT := true
-TW_INCLUDE_JB_CRYPTO := false
-TW_INCLUDE_L_CRYPTO := true
 TW_INPUT_BLACKLIST := "lsm303dlhc_acc_lt\nl3gd20_gyr\ncompass"
 TW_HAS_NO_RECOVERY_PARTITION := true
-TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
-TW_THEME := portrait_hdpi
